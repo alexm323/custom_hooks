7f23ff7018f3d82187e17f474a8457711a2ba9ff
alexm323 <alejandromartinezjr42@gmail.com>
2021-02-05 16:40:16 -0800

    intro to custom hooks

---
+++ src/MoodClicker.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './MoodClicker.css';
+import useToggleState from './hooks/useToggleState'
+
+const MoodClicker = () => {
+    const[isHappy,toggleIsHappy] = useToggleState(true)
+    const[isDarkMode,toggleIsDarkMode] = useToggleState(true)
+    return (
+        <div className={isDarkMode ? 'Clicker-dark':'Clicker-light'}>
+            <h1>{isHappy ? '😊':'😭'}</h1>
+            <button onClick={toggleIsHappy}>Change Mood</button>
+            <button onClick={toggleIsDarkMode}>Toggle Dark/Light Mode</button>
+        </div>
+    
+    )
+}
+
+export default MoodClicker;
